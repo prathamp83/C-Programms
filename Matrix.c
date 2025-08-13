d96ceacc0ddde514abad369c16a6d59e2a0722a2
prathamp83 <prathameshpawar302006@gmail.com>
2025-08-13 11:43:20 +0530

    Create Matrix.c

---
+++ Matrix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][3];
+    int i, j;
+
+    printf("Enter 9 values for a 3x3 matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("The 3x3 Matrix is:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
